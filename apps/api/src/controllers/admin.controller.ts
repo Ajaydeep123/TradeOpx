13a296232df2f157ddcf751794c40d9127cf995e
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2024-12-22 06:42:41 +0530

    Added controller logic in apps/api

--- apps/api/src/controllers/admin.controller.ts
+++ apps/api/src/controllers/admin.controller.ts
@@ -5,13 +5,86 @@ import { AsyncManager } from "../asyncmanager";
 
 
 export const createMarket = asyncHandler(async(req:Request, res: Response) =>{
+    const createMarketData = MarketSchema.safeParse(req.body);
+    if(!createMarketData.success){
+        return res.status(400).json({message:"schema validation failed!", error:createMarketData.error?.issues})
+    }
 
+    const {symbol, endTime, description, sourceOfTruth,categoryTitle}= createMarketData.data
+    try {
+        const responseFromEngine = await AsyncManager.getInstance().sendAndAwait({
+            type:'create_market',
+            payload:{
+                token:req.token!,
+                symbol,
+                endTime,
+                description,
+                sourceOfTruth,
+                categoryTitle
+            }
+        })
+
+        if(responseFromEngine.success){
+            return res.status(200).json(responseFromEngine)
+        }else{
+            return res.status(400).json(responseFromEngine)
+        }  
+    } catch (error) {
+        return res.status(500).json({success:false, message:"Internal server errror", error})
+    }
 })
 
 export const createCategory = asyncHandler(async(req:Request, res: Response) =>{
-    
+    const categoryData = CategorySchema.safeParse(req.body)
+    if(!categoryData.success){
+        return res.status(400).json({message:"schema validation failed!", error:categoryData.error?.issues})
+    }
+
+    const {title, icon, description} = categoryData.data;
+    try {
+        const responseFromEngine = await AsyncManager.getInstance().sendAndAwait({
+            type:'create_category',
+            payload:{
+                token:req.token!,
+                title,
+                icon,
+                description
+            }
+        })
+
+        if(responseFromEngine.success){
+            return res.status(200).json(responseFromEngine)
+        }else{
+            return res.status(400).json(responseFromEngine)
+        }  
+    } catch (error) {
+        return res.status(500).json({success:false, message:"Internal server errror", error})
+    }    
 })
 
 export const mintController = asyncHandler(async(req:Request, res: Response) =>{
-    
+    const mintData = MintSchema.safeParse(req.body);
+    if(!mintData.success){
+        return res.status(400).json({message:"schema validation failed!", error:mintData.error?.issues})
+    }
+
+    const {symbol, quantity, price} = mintData.data;
+    try {
+        const responseFromEngine = await AsyncManager.getInstance().sendAndAwait({
+            type:'mint',
+            payload:{
+                token:req.token!,
+                symbol,
+                quantity,
+                price
+            }
+        })
+        if(responseFromEngine.success){
+            return res.status(200).json(responseFromEngine)
+        }else{
+            return res.status(400).json(responseFromEngine)
+        }  
+    } catch (error) {
+        return res.status(500).json({success:false, message:"Internal server errror", error})
+    }    
 })
--- apps/api/src/controllers/user.controller.ts
+++ apps/api/src/controllers/user.controller.ts
@@ -5,26 +5,160 @@ import { AsyncManager } from "../asyncmanager";
 
 
 export const onRampInr = asyncHandler(async(req:Request, res: Response) =>{
+    const {amount} = req.body
 
+    if(!amount || amount<0){
+        return res.json(400).json({success: false, message: "Please send amount greater then 0 to onramp"})
+    }
+    try {
+        const responseFromEngine = await AsyncManager.getInstance().sendAndAwait({
+            type:'onramp_inr',
+            payload:{
+                token:req.token!,
+                amount
+            }
+        })
+        if(responseFromEngine.success){
+            return res.status(200).json(responseFromEngine)
+        }else{
+            return res.status(400).json(responseFromEngine)
+        }  
+    } catch (error) {
+        return res.status(500).json({success:false, message:"Internal server errror", error})
+    }
 })
 
 export const buyController = asyncHandler(async(req:Request, res: Response) =>{
-    
+    const buyData = OrderSchema.safeParse(req.body);
+    if(!buyData.success){
+        return res.status(400).json({message:"schema validation failed!", error:buyData.error?.issues})
+    }
+
+    const {symbol, quantity, price, stockType} = buyData.data;
+    try {
+        const responseFromEngine = await AsyncManager.getInstance().sendAndAwait({
+            type:'buy',
+            payload:{
+                token:req.token!,
+                symbol,
+                quantity,
+                price,
+                stockType
+            }
+        })
+        if(responseFromEngine.success){
+            return res.status(200).json(responseFromEngine)
+        }else{
+            return res.status(400).json(responseFromEngine)
+        }  
+    } catch (error) {
+        return res.status(500).json({success:false, message:"Internal server errror", error})
+    }    
 })
 
 export const sellController = asyncHandler(async(req:Request, res: Response) =>{
-    
+    const sellData = OrderSchema.safeParse(req.body);
+    if(!sellData.success){
+        return res.status(400).json({message:"schema validation failed!", error:sellData.error?.issues})
+    }
+
+    const {symbol, quantity, price, stockType} = sellData.data;
+    try {
+        const responseFromEngine = await AsyncManager.getInstance().sendAndAwait({
+            type:'sell',
+            payload:{
+                token:req.token!,
+                symbol,
+                quantity,
+                price,
+                stockType
+            }
+        })
+        if(responseFromEngine.success){
+            return res.status(200).json(responseFromEngine)
+        }else{
+            return res.status(400).json(responseFromEngine)
+        }  
+    } catch (error) {
+        return res.status(500).json({success:false, message:"Internal server errror", error})
+    }     
 })
 
 export const cancelBuy = asyncHandler(async(req:Request, res: Response) =>{
-    
+    const orderId = req.query.orderId as string;
+    const marketSymbol = req.query.marketSymbol as string
+
+    if(!orderId || !marketSymbol || (marketSymbol || orderId).length === 0){
+        return res.status(401).json({success: false, message: "Invalid request, provide valid orderId and marketSymbol as query."})
+    }
+
+    try {
+        const responseFromEngine = await AsyncManager.getInstance().sendAndAwait({
+            type:'cancel_buy_order',
+            payload:{
+                token:req.token!,
+                orderId,
+                marketSymbol
+            }
+        })
+        if(responseFromEngine.success){
+            return res.status(200).json(responseFromEngine)
+        }else{
+            return res.status(400).json(responseFromEngine)
+        }  
+    } catch (error) {
+        return res.status(500).json({success:false, message:"Internal server errror", error})
+    }    
 })
 
 
 export const cancelSell = asyncHandler(async(req:Request, res: Response) =>{
-    
+    const orderId = req.query.orderId as string;
+    const marketSymbol = req.query.marketSymbol as string
+
+    if(!orderId || !marketSymbol || (marketSymbol || orderId).length === 0){
+        return res.status(401).json({success: false, message: "Invalid request, provide valid orderId and marketSymbol as query."})
+    }
+
+    try {
+        const responseFromEngine = await AsyncManager.getInstance().sendAndAwait({
+            type:'cancel_sell_order',
+            payload:{
+                token:req.token!,
+                orderId,
+                marketSymbol
+            }
+        })
+        if(responseFromEngine.success){
+            return res.status(200).json(responseFromEngine)
+        }else{
+            return res.status(400).json(responseFromEngine)
+        }  
+    } catch (error) {
+        return res.status(500).json({success:false, message:"Internal server errror", error})
+    }     
 })
 
 export const ordersInParticularMarket = asyncHandler(async(req:Request, res: Response) =>{
-    
+    const marketSymbol = req.params.marketSymbol 
+    if(marketSymbol?.length === 0 || !marketSymbol){
+        return res.status(401).json({success:false, message:"Invalid params"})
+    }
+    try {
+        const responseFromEngine = await AsyncManager.getInstance().sendAndAwait({
+            type:'get_user_market_orders',
+            payload:{
+                token:req.token!,
+                marketSymbol
+            }
+        })
+
+        if(responseFromEngine.success){
+            return res.status(200).json(responseFromEngine)
+        }else{
+            return res.status(400).json(responseFromEngine)
+        }  
+    } catch (error) {
+        return res.status(500).json({success:false, message:"Internal server errror", error})
+    }
 })
